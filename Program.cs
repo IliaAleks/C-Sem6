bac23c511760a66ac3d8d977a15a6486a39aa978
Илья Алексеев <av98060@gmail.com>
2022-12-22 22:39:45 +0300

    Task 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//Task 41
+Console.WriteLine("Task 41");
+Console.WriteLine("введите числа через запятую и пробел и нажмите enter:");
+string masNumbString=Console.ReadLine();
+int[] numbers = masNumbString.Split(',').Select(snum => int.Parse(snum)).ToArray();
+
+int numChisel(int [] masint)
+{
+    int k=0;
+    for (int i=0; i<masint.Length; i++)
+    {
+    if (masint[i]>0) {++k;}
+    
+    }
+    return k;
+}
+Console.WriteLine($"Количесвто введеных положительных чисел = {numChisel(numbers)}");
